c120a1962b87dabcdd70ab7bcb3d01d4f70eac00
z-chubinidze <120126922+z-chubinidze@users.noreply.github.com>
2023-06-14 20:25:32 +0400

    Add files via upload

---
+++ js.hmw-23.js
@@ -0,0 +1,14 @@
+
+
+
+var masivi = [1, 67, 34, 56, 55, 10, 7, 30, 20];
+
+var indexi = 0;
+
+while(indexi <= masivi.length){
+    
+    if(masivi[indexi] % 5 == 0){
+        document.write(masivi[indexi]  + "<br>");
+    }
+    indexi++;
+}
